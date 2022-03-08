693ca29cd096a9c150afb36e81408bd7cbe78212
Praveen2173 <pk5082604@gmail.com>
2022-03-08 16:37:50 +0530

    js test

---
+++ prac.js
@@ -0,0 +1,79 @@
+'use strict'
+// var a=[1,2,3,3,4,5,6,7]; console.log(a)
+// a=[1,2,3,4,5,6,7,]
+// const myArray = [["John", 23],["dog",3]];
+// myArray.unshift(["git",27]);
+// console.log(myArray);
+// myArray
+// function reusableFunction(){
+//     console.log("Hi World")
+//   }
+ 
+// reusableFunction()
+
+// function myTest() {
+    // const loc = "foo";
+    // console.log(loc);
+//   }
+//   
+// myTest();
+// console.log(loc);
+
+
+// a=[1,2,3,4,5]
+// a.push(1)
+// console.log(a.shift(1))
+// var buf =new Buffer (10);
+// console.log(buf)
+
+// function a(x, y){
+//     return x+y
+// }
+
+// function b(i, func){
+//     let val = a(4, 7)
+//     return func(val, 4) - i
+// }
+
+     
+// b(2, function(m, n){ return m*n })
+// let i =0
+// for (let i =0;i<0;i++){
+//     console.log(i)
+// }
+// console.log(i)
+
+
+
+// Q-1  java script = java script is programig language which is a parform a object basd or purpush full task.
+// Q-2  var = var is a way of take varibal which is use as glogle based you can assing it under or extanal
+//      let or const = it is also a way of taking variable but if we take let variable in { } so they can parfom in this particular box
+// Q-3 (a)integer= +"2",22,
+//        String= "praveen"
+// 
+//      (b)    var Object = {
+//               "name": "praveen",
+//               "agr": 22,
+//               "gender":"male"
+//          }
+//          console.log(Object)
+//      (c)  var a=['praveen', 'alock']
+// 
+//Q-4 (1)
+// var a=[]
+// a.push('praveen')
+// a.push('amit')
+// a.push('vikash')
+// a.push(25)
+// a.push(34)
+// a.shift()
+// console.log(a)
+// 
+// Q-4 (2) var a={
+//          }
+// 
+// 
+// 
+// 
+// 
+// 
